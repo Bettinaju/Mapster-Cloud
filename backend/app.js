c31c49a9ff75ebd486a68e15a5719db43e020d60
lhartstein <laura.hartstein@qudosoft.de>
2024-08-30 14:28:28 +0200

    WIP: dockerfiles and mongodb-setup

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import morgan from 'morgan';
@@ -8,6 +10,34 @@ import locationsRouter from './controllers/locations.js';
 import errorHandler from './utils/errorHandler.js';
 
 const app = express();
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1); // Exit the process if the database connection fails
+    }
+};
+
+connectToDatabase();
+
+
+// Determine the directory name from import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(process.cwd(), 'public')));
+
+// Default route to serve your main HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Configure mongoDB
 mongoose
@@ -26,5 +56,10 @@ app.use('/nonsusloc', locationsRouter);
 
 // Use custom error handler
 app.use(errorHandler);
+
+// //test connection to backend for containerization
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the Mapster API');
+// });
 
 export default app;
