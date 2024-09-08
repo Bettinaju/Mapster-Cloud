57e625a0c65b07c3d8003a68b54956805c2f16ac
Bettina Justus <bettina.jus@gmail.com>
2024-09-08 18:45:03 +0200

    build: cleanup mongo docker instructions and seeding script

---
+++ mongodb/init-users.js
@@ -0,0 +1,21 @@
+// init-mongo.js
+db = db.getSiblingDB('local'); //instanciate db
+
+// Drop existing users collection if it exists
+db.users.drop();
+
+// Insert the initial data
+db.users.insertMany([
+  {
+    userId: 'admina',
+    password: 'pass1234',
+    isAdmin: true,
+  },
+  {
+    userId: 'normalo',
+    password: 'pass1234',
+    isAdmin: false,
+  },
+]);
+
+print('Initialized users collection with default data.');
